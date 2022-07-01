39d5cf3e54d82aa323fe769d1a3ca8bda613c606
Shin <jeesoshn@88665a4a6164.ant.amazon.com>
2022-07-01 12:31:23 -0700

    object enhancement first commit

---
+++ object_enhancements.js
@@ -0,0 +1,40 @@
+// same keys and values
+
+// const createInstructor =( firstName, lastName)=>{
+//     return {
+//         firstName,
+//         lastName
+//     }
+// }
+
+
+// Computed Property Names
+
+// let favoriteNumber = 42;
+// const instructor = {
+//     firstName: "Colt",
+//     [favoriteNumber] : "That is my favorite!"
+// }
+
+
+// Object Methods
+
+// let instructor = {
+//     firstName: "Colt",
+//     sayHi(){
+//         return "Hi!";
+// },
+//     sayBye(){return this.firstName + " says bye!";
+//     }
+//   }
+
+
+// const createAnimal = (species,verb,noise) =>{
+//     return {
+//         species,
+//         [verb](){
+//             return noise;
+//         }
+//     };
+
+// };
